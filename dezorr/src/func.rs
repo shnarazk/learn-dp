e81f858468893beb1ce87a844243c0eb6f55f623
Narazaki, Shuji <shujinarazaki@protonmail.com>
2023-05-09 23:42:46 +0900

    step 2:   dezorr/src/func.rs

--- dezorr/src/func.rs
+++ dezorr/src/func.rs
@@ -13,7 +13,7 @@ impl<F> Clone for Function<F> {
         }
     }
 }
-impl<D> Function<D> {
+impl<D: 'static> Function<D> {
     pub fn new(function: Box<dyn Fn(D) -> D>) -> Self {
         Function {
             function: Rc::new(function),
@@ -23,6 +23,12 @@ impl<D> Function<D> {
         let f = &*self.function;
         let n = f(v.data);
         Variable::new(n)
+    }
+    /// step 3: function composition
+    pub fn followed_by(&self, other: &Self) -> Self {
+        let f = self.function.clone();
+        let g = other.function.clone();
+        Function::<D>::new(Box::new(move |x: D| g(f(x))))
     }
 }
 
@@ -44,5 +50,16 @@ mod tests {
         assert!((f2.apply(v1.clone()).data - 7.389056).abs() < 0.001);
         let f3: Function<f32> = f2.clone();
         assert_eq!(f3.apply(v1.clone()).data, f2.apply(v1).data);
+    }
+    #[test]
+    fn test_step_3() {
+        let v1: Variable<f32> = Variable::new(1.0f32);
+        assert_eq!(v1.data, 1.0);
+        let f: Function<f32> = Function::<f32>::new(Box::new(|x: f32| x + 1.0));
+        assert_eq!(f.apply(v1.clone()).data, 2.0);
+        let g: Function<f32> = Function::<f32>::new(Box::new(|x: f32| x.exp()));
+        assert!((g.apply(v1.clone()).data - std::f32::consts::E).abs() < 0.001);
+        let fg_or_gf: Function<f32> = f.followed_by(&g);
+        assert!((fg_or_gf.apply(v1).data - std::f32::consts::E.powi(2)).abs() < 0.001);
     }
 }
